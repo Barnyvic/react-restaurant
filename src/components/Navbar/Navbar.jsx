eccc670987ff22e4e17a54680b14cd05b4b2d638
Barny victor <victorbarny4@gmail.com>
2022-03-14 07:51:18 +0100

    working on the Navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { MdOutlineRestaurantMenu } from "react-icons/md";
+
+import Logo from "../../assets/gericht.png";
+
+function Navbar() {
+  return (
+    <nav className="app__navbar">
+      <div className="app__navbar_logo">
+        <img src={Logo} alt="App-logo" />
+      </div>
+      <ul className="app__navbar_links">
+        <li className="p__opensans">
+          <a href="#Home">Home</a>
+        </li>
+        <li className="p__opensans">
+          <a href="#About">About</a>
+        </li>
+        <li className="p__opensans">
+          <a href="#Menu">Menu</a>
+        </li>
+        <li className="p__opensans">
+          <a href="#Awards">Awards</a>
+        </li>
+        <li className="p__opensans">
+          <a href="#Contact">Contact</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
