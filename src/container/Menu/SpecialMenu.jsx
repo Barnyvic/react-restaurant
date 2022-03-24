1e8ad5a06ca9be89467f498fb96913527a466706
Barny victor <victorbarny4@gmail.com>
2022-03-24 14:54:16 +0100

    created specialmemu

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -1,5 +1,20 @@
 import React from "react";
 
-const SpecialMenu = () => <div>SpecialMenu</div>;
+import { images, date } from "../../constants";
+import SubHeading from "../../components/SubHeading/SubHeading";
+
+const SpecialMenu = () => (
+  <div className="app__Specialmenu-Container">
+    <div className="app__Specialmenu-Content">
+      <SubHeading title="Menu That Fits Your Palatte" />
+      <h1>Today's Special</h1>
+    </div>
+    <div className="app__Specialmenu-subContent">
+      <div className="WineBeer"></div>
+      <div className="app__special-img"></div>
+      <div className="Cocktails"></div>
+    </div>
+  </div>
+);
 
 export default SpecialMenu;
